1468267cd8214870737a2415d95c64a2ee468456
rohan <Rohan.Girish.Aradhya@gmail.com>
2023-10-29 13:24:24 +0530

    Finished Front End

--- App/TextInputComponent.js
+++ App/TextInputComponent.js
@@ -1,17 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, TextInput, Text, StyleSheet, Dimensions } from 'react-native';
+import * as Device from 'expo-device';
+import axios from 'axios';
 
 const TextInputComponent = () => {
   const [text, setText] = useState('');
-  const [subText, setSubText] = useState('');
+  const [ansText, setAnsText] = useState('Nope');
+  const [waitFlag, setFlag] = useState(true);
+  const [myRAM, changeRAM] = useState(0);
 
   const handleTextChange = (inputText) => {
     setText(inputText);
   };
 
-  const handleTextSubmit = () => {
-    setSubText(text);
+  const handleTextSubmit = async() => {
+    try {
+      console.log(myRAM, text)
+      url = "http://192.168.196.219:5000/query"
+      const headers = {
+        'Content-Type': 'application/json', // Set the content type as needed
+      };
+  
+      // Define your request body
+      const requestBody = {
+        "question": text,
+        "RAM": myRAM
+      };
+
+      setFlag(false);
+
+      const response = await axios.post(url, requestBody, { headers });
+
+      // Handle the data from the response
+      const posts = response.data;
+      console.log('Fetched Answer:', posts);
+      setAnsText(posts)
+    } catch (error) {
+      console.error('API Error:', error);
+    }
   }
+
+  useEffect(()=>{
+    // Get RAM information
+    changeRAM(Device.totalMemory);
+  },[])
+
+  useEffect(()=>{
+    setFlag(true);
+  },[ansText])
+
+  // console.log("Here");
 
   const { width } = Dimensions.get('window');
   const inputWidth = 0.75 * width; 
@@ -24,10 +62,11 @@ const TextInputComponent = () => {
         onChangeText={handleTextChange}
         onEndEditing={handleTextSubmit} // This event is triggered when "Enter" is pressed
         value={text}
+        editable={waitFlag}
         placeholder="Type something..."
         returnKeyType="done" // This changes the return key on the keyboard to "Done"
       />
-      <Text style={styles.text}>You typed: {subText}</Text>
+      <Text style={styles.text}>Received Answer: {ansText}</Text>
     </View>
   );
 };
