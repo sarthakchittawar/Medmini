e18bcbf01a052696d336a64db7604b67fb5257c9
raghav <raghavdonakanti@gmail.com>
2023-11-10 13:05:05 +0530

    changed docker pull cmd

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-docker pull medmini
+docker pull raghav010/medmini:latest
 
-docker run -it --rm -p 8081:8081 -p 5000:5000 -e REACT_NATIVE_PACKAGER_HOSTNAME=$(hostname -I | awk '{print $1}') -e EXPO_DEVTOOLS_LISTEN_ADDRESS="0.0.0.0" medmini
+docker run -it --rm -p 8081:8081 -p 5000:5000 -e REACT_NATIVE_PACKAGER_HOSTNAME=$(hostname -I | awk '{print $1}') -e EXPO_DEVTOOLS_LISTEN_ADDRESS="0.0.0.0" raghav010/medmini:latest
